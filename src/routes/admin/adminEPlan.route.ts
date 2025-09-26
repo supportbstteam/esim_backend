744acabc8a940bf1e52f29144bae65996ba8f995
bstteam102 <bstteam102@gmail.com>
2025-09-26 22:12:12 +0530

    fix: Add comment to clarify the importance of route order in adminEPlan.route.ts

--- src/routes/admin/adminEPlan.route.ts
+++ src/routes/admin/adminEPlan.route.ts
@@ -3,7 +3,7 @@ import { createUser, getUsers } from "../../controllers/user/user.controllers";
 import { createPlan, getPlanById, getPlans, updatePlan, deletePlan } from "../../controllers/admin/adminPlans.controllers";
 
 const router = Router();
-
+// always take care of the order of routes
 router.post("/create-plan", createPlan);
 router.get("/", getPlans);
 router.get("/:planId", getPlanById);
