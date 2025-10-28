cb16e42edc4b0874970821b5458d94f01396f3e2
bstteam102 <bstteam102@gmail.com>
2025-10-28 12:15:19 +0530

    feature: user authentcation route added to controller the DB

--- src/routes/user/userAuth.route.ts
+++ src/routes/user/userAuth.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserDetails, postCreateUser, postUserLogin } from "../../controllers/user/userAuth.controllers";
+import { getUserDetails, postCreateUser, postForgotPassword, postResetPassword, postUserLogin, postVerifyForgotPasswordOtp } from "../../controllers/user/userAuth.controllers";
 
 // import { signup, login, getUserDetails } from "../controllers/user.controllers";
 // import { authMiddleware } from "../middleware/auth";
@@ -9,5 +9,8 @@ const router = express.Router();
 // Public
 router.post("/signup", postCreateUser);
 router.post("/login", postUserLogin);
+router.post("/forget-password", postForgotPassword);
+router.post("/forget-password-otp", postVerifyForgotPasswordOtp);
+router.post("/temp-reset-password", postResetPassword);
 
 export default router;
