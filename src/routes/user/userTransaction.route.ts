80ad20475fb4d5d4d471ecb8a861a7004911e629
bstteam102 <bstteam102@gmail.com>
2025-11-25 14:56:04 +0530

    refactor: un comment stripe initiate

--- src/routes/user/userTransaction.route.ts
+++ src/routes/user/userTransaction.route.ts
@@ -4,14 +4,12 @@ import { auth } from '../../middlewares/auth.handler';
 
 const router = express.Router();
 
-// router.post("/stripe/initiate", initiateTransaction);
+router.post("/stripe/initiate", initiateTransaction);
 router.post("/cod/initiate", handleCODTransaction);
 router.get("/", getUserTransactions);
 router.post("/topup/initiate", initiateTopUpTransaction);
 
 // Update transaction status manually (from frontend)
 router.post("/:id/success", handleTransactionStatus);
-
-
 
 export default router;
