7d39ff3afa35949d50529abf59c29e85b9296b1a
bstteam102 <bstteam102@gmail.com>
2025-11-27 15:51:34 +0530

    refactor: smtp user

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -5,13 +5,12 @@ import { Admin } from "../entity/Admin.entity";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: Number(process.env.SMTP_PORT) === 465,
+  secure: true, // IMPORTANT for port 465
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
-
 
 const adminMailNotfication = async () => {
 
