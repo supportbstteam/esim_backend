05eda0a288e8b816330dc49930666bdab1d0cbee
supportbstteam <support@betasofttechnology.com>
2026-01-19 12:56:55 +0530

    refactor: adding country details for iso and iso3

--- src/controllers/user/userPlans.controllers.ts
+++ src/controllers/user/userPlans.controllers.ts
@@ -50,7 +50,9 @@ export const getUserPlans = async (req:any, res: Response) => {
       country: {
         id: plan.country.id,
         name: plan.country.name,
-        description:plan?.country?.description
+        description:plan?.country?.description,
+        ios: plan?.country?.isoCode,
+        iso3Code: plan?.country?.iso3Code,
       },
       createdAt: plan.createdAt,
       updatedAt: plan.updatedAt,
