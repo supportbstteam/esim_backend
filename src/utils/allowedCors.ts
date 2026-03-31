c8510bc3ac0502d79bc2132428b2308c268690ca
supportbstteam <support@betasofttechnology.com>
2026-03-31 17:50:46 +0530

    refactor: added the cors and to publish on vercel

--- src/utils/allowedCors.ts
+++ src/utils/allowedCors.ts
@@ -4,12 +4,7 @@ export const ALLOWED_PATH_ORIGINS = [
   "https://esim-admin-peach.vercel.app",
   "https://loadbalancer.turisim.com",
   "https://onesignal.com",
-  "http://192.168.29.89:3000",
   "https://www.esimaero.com",
   "https://admin.esimaero.com",
   "https://api.esimaero.com",
-  "http://192.168.1.9:3000",
-  "http://192.168.1.51:3000",
-  "http://192.168.1.3:3000",
-  "http://192.168.29.89:3000"
 ];
